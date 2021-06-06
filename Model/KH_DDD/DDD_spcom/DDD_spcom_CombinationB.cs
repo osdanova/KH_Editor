a37a708462070eb45dba1ead387557a111dac8ae
osdanova <osdanovaonline@outlook.com>
2021-06-06 10:41:38 +0200

    spcom tableB

--- Model/KH_DDD/DDD_spcom/DDD_spcom_CombinationB.cs
+++ Model/KH_DDD/DDD_spcom/DDD_spcom_CombinationB.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using KH_Editor.Dictionaries.KH_DDD;
 using Xe.BinaryMapper;
 
 namespace KH_Editor.Model.KH_DDD.DDD_spcom
@@ -9,6 +9,21 @@ namespace KH_Editor.Model.KH_DDD.DDD_spcom
     {
         public static readonly int SIZE = 16;
 
-        [Data(Count=16)] public List<byte> unk { get; set; }
+        [Data] public byte itemId { get; set; }
+        [Data] public byte itemCategory { get; set; }
+        [Data] public short hp { get; set; }
+        [Data] public byte attack { get; set; }
+        [Data] public byte defense { get; set; }
+        [Data] public byte magic { get; set; }
+        [Data] public byte ele_fire { get; set; }
+        [Data] public byte ele_blizzard { get; set; }
+        [Data] public byte ele_thunder { get; set; }
+        [Data] public byte ele_water { get; set; }
+        [Data] public byte ele_dark { get; set; }
+        [Data] public byte ele_light { get; set; }
+        [Data] public byte affinity { get; set; }
+        [Data] public short padding { get; set; }
+
+        public string itemName { get { return DDD_Items.getItemNameById(itemId, itemCategory); } }
     }
 }
